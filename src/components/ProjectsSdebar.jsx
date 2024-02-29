d58c4ba1822b38cf0ef3f40677cb4f5e054356c3
Iris Yatskaer <iriska1160@gmail.com>
2024-02-29 13:24:28 +0200

    Add new project and show it on sidebar

--- src/components/ProjectsSdebar.jsx
+++ src/components/ProjectsSdebar.jsx
@@ -1,6 +1,6 @@
 import Button from "./Buttons";
 
-export default function ProjectsSidebar({ onStartAddProject }) {
+export default function ProjectsSidebar({ onStartAddProject, projects }) {
 
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
@@ -12,20 +12,17 @@ export default function ProjectsSidebar({ onStartAddProject }) {
         <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
       <ul className="mt-8">
-        <li className="flex justify-between my-4">
-          <button
-            className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800"
-          >
-            Learning React
-          </button>
-        </li>
-        <li className="flex justify-between my-4">
-          <button
-            className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800"
-          >
-            Mastering React
-          </button>
-        </li>
+        {projects.map(project => {
+          return (
+            <li key={project.id} className="flex justify-between my-4">
+              <button
+                className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800"
+              >
+                {project.title}
+              </button>
+            </li>
+          )
+        })}
       </ul>
     </aside>
 
